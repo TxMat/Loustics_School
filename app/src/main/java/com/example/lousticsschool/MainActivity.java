10a3663cc7cddd5d854392d7b6c86681ed0a5119
ponalm <mathieu.ponal@etu.univ-grenoble-alpes.fr>
2022-03-24 11:23:34 +0100

    Revert "basic math activity"
    This reverts commit 62ef466c

--- app/src/main/java/com/example/lousticsschool/MainActivity.java
+++ app/src/main/java/com/example/lousticsschool/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.lousticsschool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.btn1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MathActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
