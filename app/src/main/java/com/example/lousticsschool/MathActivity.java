ac361f28be250729b187e8a6213174ee3288d5f4
ponalm <mathieu.ponal@etu.univ-grenoble-alpes.fr>
2022-04-10 01:25:18 +0200

    formatting

--- app/src/main/java/com/example/lousticsschool/MathActivity.java
+++ app/src/main/java/com/example/lousticsschool/MathActivity.java
@@ -1,7 +1,5 @@
 package com.example.lousticsschool;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
@@ -15,11 +13,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.lousticsschool.db.AppDb;
 import com.example.lousticsschool.db.User;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MathActivity extends AppCompatActivity {
 
@@ -59,7 +58,6 @@ public class MathActivity extends AppCompatActivity {
         }
 
 
-
         Calc = findViewById(R.id.MathTextView);
         Answer = findViewById(R.id.Answer);
         Previous = findViewById(R.id.Previous);
@@ -95,13 +93,13 @@ public class MathActivity extends AppCompatActivity {
             builder.setTitle("Abandonner");
             builder.setMessage("Voulez-vous vraiment abandonner ?\nVous perdrez toutes vos réponses.");
             builder.setPositiveButton("Oui, je veux quitter l'exercice", (dialog, which) -> {
-                Intent intent = new Intent(getApplicationContext(), LoggedActivity.class);
-                //clear all the activity stack
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.putExtra("user", current_user);
-                startActivity(intent);
-                finish();
-            }
+                        Intent intent = new Intent(getApplicationContext(), LoggedActivity.class);
+                        //clear all the activity stack
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        intent.putExtra("user", current_user);
+                        startActivity(intent);
+                        finish();
+                    }
             );
             builder.setNegativeButton("Non, je veux continuer", (dialog, which) -> {
                 dialog.dismiss();
@@ -113,83 +111,71 @@ public class MathActivity extends AppCompatActivity {
     }
 
     private boolean CheckResult(String answer) {
-        if (Answer.isEnabled()) {
-            try {
-                if (mathModel.IsCorrect(answer)) {
-                    Answer.setTextColor(Color.GREEN);
-                    Calc.setTextColor(Color.GREEN);
-                    // increment user correct answers
-                    current_user.setTotalQuestionsCorrect(current_user.getTotalQuestionsCorrect() + 1);
+        try {
+            if (mathModel.IsCorrect(answer)) {
+                Answer.setTextColor(Color.GREEN);
+                Calc.setTextColor(Color.GREEN);
+                // increment user correct answers
+                current_user.setTotalQuestionsCorrect(current_user.getTotalQuestionsCorrect() + 1);
 
-                } else {
-                    // set the text to red if the user has entered the wrong answer and display the correct answer
-                    Answer.setTextColor(Color.RED);
-                    Calc.setTextColor(Color.RED);
-                    QALayout.startAnimation(AnimationUtils.loadAnimation(this,R.anim.shake));
-                }
-                Next.setEnabled(false);
-                Previous.setEnabled(false);
-                Answer.setEnabled(false);
-                Quit.setEnabled(false);
-                mathModel.setCurrentAnswer(answer);
-                // increment user total questions answered
-                current_user.setTotalQuestionsAnswered(current_user.getTotalQuestionsAnswered() + 1);
-                updateUser();
-                // wait for 1 second
-                Handler handler = new Handler();
-                handler.postDelayed(() -> {
-                    // if the user has answered all the questions fininsh the activity and clear the activity stack
-                    if (mathModel.isLastQuestion()) {
-                        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                        // concatenate question array with answer array in an other array and send it to the result activity
-                        ArrayList<String> result = new ArrayList<>();
-                        for (int i = 0; i < mathModel.getTotalQuestionsNb(); i++) {
-                            result.add(mathModel.GetCalculArray().get(i) + " = " + mathModel.GetAnswerArray().get(i));
-                        }
-                        intent.putExtra("RESULT_ARRAY", result);
-                        intent.putExtra("EXERCICE_TYPE", "Math");
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        // if the user has not answered all the questions, go to the next question
-                        Intent intent = new Intent(getApplicationContext(), MathActivity.class);
-                        intent.putExtras(mathModel.getNextBundle());
-                        intent.putExtra("user", current_user);
-                        // add a slide animation when starting the next activity
-                        startActivity(intent);
-                        overridePendingTransition(R.anim.slide_in_right, R.anim.no_transition);
-                        Next.setEnabled(true);
-                        Previous.setEnabled(mathModel.getCurrentQuestionNb() != 1);
-                        Quit.setEnabled(true);
-                        finish();
-                    }
-
-                }, 1000);
-                return true;
-            } catch (IllegalArgumentException e) {
-                Toast.makeText(this, "Veuillez entrer une réponse", Toast.LENGTH_SHORT).show();
-                return false;
-            } catch (Exception e) {
-                Toast.makeText(this, "Une erreur est survenue", Toast.LENGTH_SHORT).show();
-                throw e;
+            } else {
+                // set the text to red if the user has entered the wrong answer and display the correct answer
+                Answer.setTextColor(Color.RED);
+                Calc.setTextColor(Color.RED);
+                QALayout.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake));
             }
-        } else {
-            // go to perviouly exited activity
-            Intent intent = new Intent(getApplicationContext(), MathActivity.class);
-            intent.putExtras(mathModel.getBundle());
+            Next.setEnabled(false);
+            Previous.setEnabled(false);
+            Answer.setEnabled(false);
+            Quit.setEnabled(false);
+            mathModel.setCurrentAnswer(answer);
+            // increment user total questions answered
+            current_user.setTotalQuestionsAnswered(current_user.getTotalQuestionsAnswered() + 1);
+            updateUser();
+            // wait for 1 second
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                // if the user has answered all the questions fininsh the activity and clear the activity stack
+                if (mathModel.isLastQuestion()) {
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    // concatenate question array with answer array in an other array and send it to the result activity
+                    ArrayList<String> result = new ArrayList<>();
+                    for (int i = 0; i < mathModel.getTotalQuestionsNb(); i++) {
+                        result.add(mathModel.GetCalculArray().get(i) + " = " + mathModel.GetAnswerArray().get(i));
+                    }
+                    intent.putExtra("RESULT_ARRAY", result);
+                    intent.putExtra("EXERCICE_TYPE", "Math");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // if the user has not answered all the questions, go to the next question
+                    Intent intent = new Intent(getApplicationContext(), MathActivity.class);
+                    intent.putExtras(mathModel.getNextBundle());
+                    intent.putExtra("user", current_user);
+                    // add a slide animation when starting the next activity
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.no_transition);
+                    Next.setEnabled(true);
+                    Previous.setEnabled(mathModel.getCurrentQuestionNb() != 1);
+                    Quit.setEnabled(true);
+                    finish();
+                }
 
-            // add a slide animation when starting the next activity
-            startActivity(intent);
-            overridePendingTransition(R.anim.slide_in_right, R.anim.no_transition);
+            }, 1000);
             return true;
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(this, "Veuillez entrer une réponse", Toast.LENGTH_SHORT).show();
+            return false;
+        } catch (Exception e) {
+            Toast.makeText(this, "Une erreur est survenue", Toast.LENGTH_SHORT).show();
+            throw e;
         }
     }
 
     // Async class update user int the database
     private void updateUser() {
-        ///////////////////////
-        // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
+
         class UpdateUser extends AsyncTask<Void, Void, Void> {
 
 
@@ -200,13 +186,9 @@ public class MathActivity extends AppCompatActivity {
             }
         }
 
-        //////////////////////////
-        // IMPORTANT bien penser à executer la demande asynchrone
-        // Création d'un objet de type GetTasks et execution de la demande asynchrone
         UpdateUser gu = new UpdateUser();
         gu.execute();
     }
-
 
 
 }
